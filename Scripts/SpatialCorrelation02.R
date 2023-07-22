bd316fa733678d15a0511d0196d64a15a329a718
Yacouba <yacoubu02@gmail.com>
2023-07-22 00:57:49 +0000

    Update

---
+++ Scripts/SpatialCorrelation02.R
@@ -0,0 +1,142 @@
+
+rm(list = ls())
+library(rio)
+library(rgdal)
+library(dplyr)
+library(sf)
+library(mapview)
+library(tmap)
+
+library(stringi)
+setwd("~/Desktop/BioD/")
+df = rio::import("Migrations_Works/Data/situation-des-pdi-par-communes-accueil-du-28-fevrier-2023.xlsx")
+# remove the first roows
+df <- slice(df, -(1:5))
+colnames(df) <- df[1, ]
+df <- slice(df, -1)
+df <- slice(df,-c((nrow(df) - 1):nrow(df)))
+#df$COMMUNES <- tolower(df$COMMUNES)
+df$PROVINCES = paste(toupper(substring(df$PROVINCES, 1, 1)), tolower(substring(df$PROVINCES, 2)), sep = "")
+
+
+BFShapeFile<-readOGR("Data/gadm41_BFA_shp/gadm41_BFA_2.shp")
+shapefile=st_read("Data/gadm41_BFA_shp/gadm41_BFA_2.shp")
+DataShapefile= as.data.frame(shapefile)
+
+# Supprimer les accents de la colonne "votre_colonne" dans le dataframe "df"
+DataShapefile$NAME_2 <- stri_trans_general(DataShapefile$NAME_2, "Latin-ASCII")
+
+colnames(df)[colnames(df) == "PROVINCES"] <- "NAME_2"
+df$NAME_2 <- gsub("Komondjari", "Komandjoari", df$NAME_2)
+
+
+File<-df%>%
+  group_by(NAME_2)%>%
+  summarise(TotalIDP=sum(as.numeric(`Nombre total de PDI`)) )
+dd=merge(DataShapefile,File, by = "NAME_2",all.x = TRUE)
+
+File<-df%>%
+  group_by(NAME_2)%>%
+  summarise(Femelle=sum(as.numeric(Femmes)) )
+dd=merge(dd,File, by = "NAME_2",all.x = TRUE)
+
+File<-df%>%
+  group_by(NAME_2)%>%
+  summarise(Man=sum(as.numeric(Hommes)) )
+dd=merge(dd,File, by = "NAME_2",all.x = TRUE)
+
+File<-df%>%
+  group_by(NAME_2)%>%
+  summarise(ChildrenUnder_5=sum(as.numeric(`Enfants de moins de 5 ans`)) )
+dd=merge(dd,File, by = "NAME_2",all.x = TRUE)
+
+File<-df%>%
+  group_by(NAME_2)%>%
+  summarise(ChildrenOver_5=sum(as.numeric(`Enfants de plus de 5 ans`)) )
+dd=merge(dd,File, by = "NAME_2",all.x = TRUE)
+
+File<-df%>%
+  group_by(NAME_2)%>%
+  summarise(TotalChildren=sum(as.numeric(`Total Enfants`)) )
+dd=merge(dd,File, by = "NAME_2",all.x = TRUE)
+
+
+dd_sf <- st_as_sf(dd)
+
+
+
+nb <- poly2nb(dd_sf, queen = TRUE) # queen shares point or border
+nbw <- nb2listw(nb, style = "W")
+
+
+dd_sf$TotalIDP[is.na(dd_sf$TotalIDP)] <- 0
+lmoran <- localmoran(dd_sf$TotalIDP, nbw, alternative = "greater")
+head(lmoran)
+
+tmap_mode("view")
+
+dd_sf$lmI <- lmoran[, "Ii"] # local Moran's I
+dd_sf$lmZ <- lmoran[, "Z.Ii"] # z-scores
+# p-values corresponding to alternative greater
+dd_sf$lmp <- lmoran[, "Pr(z > E(Ii))"]
+
+p1 <- tm_shape(dd_sf) +
+  tm_polygons(col = "TotalIDP", title = "vble", style = "quantile") +
+  tm_layout(legend.outside = TRUE)
+
+p2 <- tm_shape(dd_sf) +
+  tm_polygons(col = "lmI", title = "Local Moran's I",
+              style = "quantile") +
+  tm_layout(legend.outside = TRUE)
+
+p3 <- tm_shape(dd_sf) +
+  tm_polygons(col = "lmZ", title = "Z-score",
+              breaks = c(-Inf, 1.65, Inf)) +
+  tm_layout(legend.outside = TRUE)
+
+p4 <- tm_shape(dd_sf) +
+  tm_polygons(col = "lmp", title = "p-value",
+              breaks = c(-Inf, 0.05, Inf)) +
+  tm_layout(legend.outside = TRUE)
+
+tmap_arrange(p1, p2, p3, p4)
+
+mp <- moran.plot(as.vector(scale(dd_sf$TotalIDP)), nbw)
+
+tm_shape(dd_sf) + tm_polygons(col = "lmZ",
+                            title = "Local Moran's I", style = "fixed",
+                            breaks = c(-Inf, -1.96, 1.96, Inf),
+                            labels = c("Negative SAC", "No SAC", "Positive SAC"),
+                            palette =  c("blue", "white", "red")) +
+  tm_layout(legend.outside = TRUE)
+
+
+dd_sf$quadrant <- NA
+# high-high
+dd_sf[(mp$x >= 0 & mp$wx >= 0) & (dd_sf$lmp <= 0.05), "quadrant"]<- 1
+# low-low
+dd_sf[(mp$x <= 0 & mp$wx <= 0) & (dd_sf$lmp <= 0.05), "quadrant"]<- 2
+# high-low
+dd_sf[(mp$x >= 0 & mp$wx <= 0) & (dd_sf$lmp <= 0.05), "quadrant"]<- 3
+# low-high
+dd_sf[(mp$x <= 0 & mp$wx >= 0) & (dd_sf$lmp <= 0.05), "quadrant"]<- 4
+# non-significant
+dd_sf[(dd_sf$lmp > 0.05), "quadrant"] <- 5
+
+
+
+
+tm_shape(dd_sf) + tm_fill(col = "quadrant", title = "",
+                        breaks = c(1, 2, 3, 4, 5, 6),
+                        palette =  c("red", "blue", "lightpink", "skyblue2", "white"),
+                        labels = c("High-High", "Low-Low", "High-Low",
+                                   "Low-High", "Not significant")) +
+  tm_legend(text.size = 1)  + tm_borders(alpha = 0.5) +
+  tm_layout(frame = FALSE,  title = "Clusters")  +
+  tm_layout(legend.outside = TRUE)
+
+
+#High-High: areas of high values with neighbors of high values,
+#High-Low: areas of high values with neighbors of low values,
+#Low-High: areas of low values with neighbors of high values,
+#Low-Low: areas of low values with neighbors of low values.
--- Scripts/migrationFlow02.R
+++ Scripts/migrationFlow02.R
@@ -9,8 +9,7 @@ library(rgdal)
 library(sf)
 library(leaflet)
 library(ggplot2)
-library(odf)
-library(donutmaps)
+
 df =data = rio::import("Migrations_Works/Data/population.csv")
 
 
