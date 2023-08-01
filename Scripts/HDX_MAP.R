b8bd7a7ca32263d3b223791a23fe6107987c1495
Yacouba <yacoubu02@gmail.com>
2023-08-01 11:16:35 +0000

    Update

--- Scripts/HDX_MAP.R
+++ Scripts/HDX_MAP.R
@@ -13,24 +13,9 @@ df <- df %>%
                        "N'Guiguimi" = "N'Guigmi",
                        "Niamey" = "Ville de Niamey"))
 
-df2=rio::import("Migrations_Works/Data/HDX/DataOk/Apr2023.xlsx")
-df2 =df2%>%
-  group_by(DEPARTEMENT)%>%
-  summarise(Apr2023 = sum(Personnes))
-colnames(df2)[1]= "adm_02"
-df2 <- df2 %>%
-  mutate(adm_02 = recode(adm_02,
-                         "Ayérou" = "Ayerou",
-                         "Gotheye" = "Gothèye",
-                         "Maine-Soroa" = "Maïné Soroa",
-                         "Maradi" = "Ville de Maradi",
-                         "N'Guiguimi" = "N'Guigmi",
-                         "Niamey" = "Ville de Niamey",
-                         "Tera"= "Téra"))
-
 df3 <- merge(df, df2, by = "adm_02", all = TRUE)
 
-Data_Sums <- merge(shapefile, df3, by = "adm_02", all = TRUE)
+Data_Sums <- merge(shapefile, df, by = "adm_02", all = TRUE)
 colnames(Data_Sums)[11:36] = c("t2016","t2017","t2018","t2019","t2020","Jan-2021","Feb2021","Mar-2021","Apr-2021","May-2021",
                                "Jun-2021","Jul-2021","Aug-2021","Sep-2021","Oct-2021","Nov-2021","Dec-2021",
                                "Jav-2022","Feb-2022","Mar-2022","Apr-2022","May-2022","Jun-2022","Jul-2022","Aug-2022","Sep-2022")
@@ -99,6 +84,21 @@ shinyApp(ui, server)
 
 
 
+
+df2=rio::import("Migrations_Works/Data/HDX/DataOk/Apr2023.xlsx")
+df2 =df2%>%
+  group_by(DEPARTEMENT)%>%
+  summarise(Apr2023 = sum(Personnes))
+colnames(df2)[1]= "adm_02"
+df2 <- df2 %>%
+  mutate(adm_02 = recode(adm_02,
+                         "Ayérou" = "Ayerou",
+                         "Gotheye" = "Gothèye",
+                         "Maine-Soroa" = "Maïné Soroa",
+                         "Maradi" = "Ville de Maradi",
+                         "N'Guiguimi" = "N'Guigmi",
+                         "Niamey" = "Ville de Niamey",
+                         "Tera"= "Téra"))
 
 
 
